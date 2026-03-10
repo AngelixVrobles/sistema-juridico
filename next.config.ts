264d276a134a556fc74b0df523ca670572fffc9c
Michael-Ucne <michael_jose@ucne.edu.do>
2026-03-10 16:38:11 -0400

    Refactor next.config.ts for turbopack and external packages
    Updated next.config.ts to conditionally include turbopack for development only and added serverExternalPackages.

--- next.config.ts
+++ next.config.ts
@@ -7,14 +7,20 @@ const nextConfig: NextConfig = {
   // Standalone output para el build de Electron (incluye server.js autónomo)
   ...(isElectronBuild && { output: "standalone" }),
 
-  turbopack: {
-    root: path.resolve(__dirname),
-  },
+  // Turbopack SOLO para Dev, No para produccion.
+  ...(!isElectronBuild && {
+    turbopack: {
+      root: path.resolve(__dirname),
+    },
+  }),
 
   // Permitir que Electron sirva los archivos de subida desde userData en producción
   images: {
     unoptimized: true,
   },
+
+  // Deben de matenerlo externo para evitar que Prisma falle en el empaquetado.
+  serverExternalPackages: ["@prisma/client", "prisma"],
 };
 
 export default nextConfig;
